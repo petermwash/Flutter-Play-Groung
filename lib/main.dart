55713a08a55925e15116e598342dde3e26d3d318
petermwash <petermwash48@gmail.com>
2019-05-03 16:08:25 +0300

    (Working with a long list of items)
    - Prepare the Data Source
    - Convert Data Source into Widgets
    - Use Widgets as children of a ListView

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,35 @@ import 'package:flutter_app/app_screens/home.dart';
 void main() {
   runApp(MaterialApp(
     title: "Exploring UI widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Long List"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: getListView(),
+    ),
   ));
+}
+
+/// Here we are preparing the Data Source
+List<String> getListItems() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+
+  return items;
+}
+
+/// Here we are converting the Data Source into widgets
+/// Then Using them as children of a ListView
+Widget getListView() {
+  var listItems = getListItems();
+
+  var listView = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      leading: Icon(Icons.art_track),
+      title: Text(listItems[index]),
+      onTap: () => debugPrint("${listItems[index]} was TAPPED!!"),
+    );
+  });
+
+  return listView;
 }
