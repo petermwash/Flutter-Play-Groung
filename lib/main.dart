649bf68afdfee436362d31e7703e8d8fb1586f2a
petermwash <petermwash48@gmail.com>
2019-05-03 13:57:33 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app_screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Exploring UI widgets",
+    home: Home(),
+  ));
+}
