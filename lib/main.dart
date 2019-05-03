3742dda8b07e005828e325ee56ec87ea869a3a9e
petermwash <petermwash48@gmail.com>
2019-05-03 15:29:55 +0300

    Using Basic ListView

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,40 @@ import 'package:flutter_app/app_screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Exploring UI widgets",
-    home: Home(),
+    title: "Exploring ListView",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Basic ListView"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: getListView(),
+    ),
   ));
+}
+
+Widget getListView() {
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.laptop_mac),
+        title: Text("Laptop Mac"),
+        subtitle: Text("MacBook 2017"),
+        trailing: Icon(Icons.laptop_mac),
+        onTap: () => debugPrint(
+          "Laptop Mac was tapped!!"
+        ),
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_windows),
+        title: Text("Laptop Windows"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      ),
+      Text("Yet anothe element of the list"),
+      Container(color: Colors.blueGrey, height: 50.0,)
+    ],
+  );
+  return listView;
 }
