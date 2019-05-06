06eb888371793fa5df3ac991deea8ec2cd9465a3
petermwash <petermwash48@gmail.com>
2019-05-06 15:23:07 +0300

    (Stateful Widgets)
    - Create a class that extends a Stetefulwidget and returns a State in the onCreateState method
    - Ceate a State class (that extends the parent class State) with properties that may change in future
    - Within the State class implement the build method to retorn a Widget
    - Call the setState method to make the changes reflect on the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/app_screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Exploring UI widgets",
-    home: Home(),
+    title: "Stateful Widget App",
+    home: FavoriteCity(),
   ));
+}
+
+class FavoriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoriteCityState();
+  }
+}
+
+class _FavoriteCityState extends State<FavoriteCity> {
+  String cityName = "";
+
+  @override
+  Widget build(BuildContext context) {
+
+    debugPrint("Favourite City widget is created");
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Favourite City"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              ///You can use onSubmitted()
+              onChanged: (String userInput) {
+                setState(() {
+                  debugPrint("setState() is called, this tells the framework to redraw the FavoriteCity widget");
+                  cityName = userInput;
+                });
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                "Your Favourite city is $cityName",
+                style: TextStyle(fontSize: 20.0, color: Colors.indigo),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
